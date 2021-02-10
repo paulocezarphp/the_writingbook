d1e43f60cddcabe27e6e4dc2da4a4611a4ee939b
Cezar Leigos <cezarleigos28@outlook.com>
2021-02-10 14:13:06 -0300

    v0.01 - Creating the basic structure
    this time we created the basic structure that we will use to develop our system in a simple and objective way.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Writing Book</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="sortcut icon" href="app/images/icon.png" type="image/x-icon" />
+	<link rel="stylesheet" type="text/css" href="app/css/style.css">
+	<link rel="stylesheet" type="text/css" href="app/css/style_mobile.css">
+    <link rel="stylesheet" type="text/css" href="app/css/style_desktop.css">
+</head>
+<body>
+
+	<?php
+        
+        $user_login = false;
+        if($user_login == true){
+
+        }else{
+
+        }  
+
+	?>
+
+</body>
+</html>
